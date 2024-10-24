f11b3387bc8932e9bd200749323338f1af6b4fe7
uma <udayarekhachowdary96@gmail.com>
2024-10-24 12:48:57 +0530

    new

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-import Navbar from '../components/Navbar';
+// index.js (Home Page)
+export default function Home() {
+  return (
+    <div className="home-container">
+      <nav className="navbar">
+        <ul>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/register">Register</a></li>
+          <li><a href="/dashboard">Dashboard</a></li>
+        </ul>
+      </nav>
 
-const Home = () => (
-  <div>
-    <Navbar />
-    <h1>Welcome to the Contact Manager App</h1>
-  </div>
-);
-
-export default Home;
+      <h1 className="home-heading">Welcome to the Contact Manager App</h1>
+      <p className="home-subheading">Manage your contacts efficiently and securely</p>
+      <button className="home-button">Get Started</button>
+    </div>
+  );
+}
