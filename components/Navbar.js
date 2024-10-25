3469ccd8d0b0ef83c42b790b1b526c591ee6aa83
uma <udayarekhachowdary96@gmail.com>
2024-10-25 10:36:26 +0530

    new

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => {
     <nav className="navbar">
       <ul>
         <li><Link href="/">Home</Link></li>
-        <li><Link href="/login">Login</Link></li>
         <li><Link href="/register">Register</Link></li>
+        <li><Link href="/login">Login</Link></li>
         <li><Link href="/dashboard">Dashboard</Link></li>
       </ul>
     </nav>
